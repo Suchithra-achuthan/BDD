07142c7e1302681b071befa4879af87ca375c426
suchithra-achuthan <suchithra.achuthan@gmail.com>
2020-07-14 11:53:09 -0400

    Commented Tags in TestRunner

--- src/test/java/runnner/BorlandTestRunner.java
+++ src/test/java/runnner/BorlandTestRunner.java
@@ -4,7 +4,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 @CucumberOptions(
 		format = {"pretty","html:Reports/Cucumber-reports/Cucumber-pretty"},
-		tags = {"@QuantityDetails"},
+	//	tags = {"@QuantityDetails"},
 		features = "src/test/java/features",
 		glue = {"stepdefinitions"}
 		)
